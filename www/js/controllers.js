7c7f62c3f331bf1535237aa0a828a8b9be7296c7
maxxlayen <maxnatsu@live.com>
2016-06-02 22:29:11 -0300

    refatoração

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,37 +1,7 @@
 angular.module('app.controllers', [])
 
 .controller('inicioCtrl', function($scope) {
-
-      $scope.iniciarJogo = function(){
-        sessionStorage.primeira = "";
-        sessionStorage.segunda = "";
-        sessionStorage.terceira = "";
-
-        //random das fáceis
-        var json = JSON.parse(sessionStorage.perguntasFaceis);
-        var random = Math.floor((Math.random() * json.length));
-        sessionStorage.primeira = random;
-        random = Math.floor((Math.random() * json.length));
-        while(random === sessionStorage.primeira){
-          random = Math.floor((Math.random() * json.length));
-        }
-        sessionStorage.segunda = random;
-        var igual;
-        do {
-          random = Math.floor((Math.random() * json.length));
-          if (random == sessionStorage.primeira || random == sessionStorage.segunda) {
-            igual = true;
-          } else {
-            igual = false;
-          }
-        } while (igual);
-        sessionStorage.terceira = random;
-
-        sessionStorage.questaoAtual = 1;
-
-        document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-balanced"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Fácil</h1> </div>';
-        document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + json[sessionStorage.primeira].pergunta + '</p>';
-      }
+    
 
 })
 
@@ -40,20 +10,107 @@ angular.module('app.controllers', [])
 })
 
 
-.controller('questoesCtrl', function($scope) {
-  var jsonF = JSON.parse(sessionStorage.perguntasFaceis);
-  var jsonM = JSON.parse(sessionStorage.perguntasMedias);
-  var jsonD = JSON.parse(sessionStorage.perguntasDificeis);
+.controller('questoesCtrl', function($scope) {  
+   
+    document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-balanced"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Fácil</h1> </div>';
+    document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + json[localStorage.primeira].pergunta + '</p>';
+ 
 
   $scope.irProximaQuestao = function(){
-    var valor = document.getElementById("resposta").value;
-    /*  if(valor == json[sessionStorage.primeiraFacil].resposta){
-        alert("resposta correta!");
-      }else{
-        alert("errou!!");
-      } */
+      console.log("deveria chamar a próxima questão.");
+
+        var valor = document.getElementById("resposta").value;
+        var jsonF = JSON.parse(localStorage.perguntasFaceis);
+        var jsonM = JSON.parse(localStorage.perguntasMedias);
+        var jsonD = JSON.parse(localStorage.perguntasDificeis);
 
 
-  }
+        if(localStorage.questaoAtual == 1){
+            if(valor == jsonF[localStorage.primeira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-balanced"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Fácil</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonF[localStorage.segunda].pergunta + '</p>';
+                localStorage.questaoAtual = 2;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 2){
+            if(valor == jsonF[localStorage.segunda].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-balanced"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Fácil</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonF[localStorage.terceira].pergunta + '</p>';
+                localStorage.questaoAtual = 3;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 3) {
+            if(valor == jsonF[localStorage.terceira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-energized"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Médio</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonM[localStorage.primeira].pergunta + '</p>';
+                localStorage.questaoAtual = 4;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 4) {
+            if(valor == jsonM[localStorage.primeira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-energized"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Médio</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonM[localStorage.segunda].pergunta + '</p>';
+                localStorage.questaoAtual = 5;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 5) {
+            if(valor == jsonM[localStorage.segunda].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-energized"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Médio</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonM[localStorage.terceira].pergunta + '</p>';
+                localStorage.questaoAtual = 6;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        }  else if (localStorage.questaoAtual == 6) {
+            if(valor == jsonM[localStorage.terceira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-royal"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Difícil</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonD[localStorage.primeira].pergunta + '</p>';
+                localStorage.questaoAtual = 7;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 7) {
+            if(valor == jsonD[localStorage.primeira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-royal"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Difícil</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonD[localStorage.segunda].pergunta + '</p>';
+                localStorage.questaoAtual = 8;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 8) {
+            if(valor == jsonD[localStorage.segunda].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-royal"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >Difícil</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' + jsonD[localStorage.terceira].pergunta + '</p>';
+                localStorage.questaoAtual = 9;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        } else if (localStorage.questaoAtual == 9) {
+            if(valor == jsonD[localStorage.terceira].resposta){
+                document.getElementById("barraDeNivel").innerHTML = '<div class="bar bar-header bar-dark"><button class="button icon" ui-sref="inicio"></button><h1 class="title" >DESAFIO!</h1> </div>';
+                document.getElementById("perguntas").innerHTML = '<p class="perguntar">' +'<img src="../img/DESAFIO.png">' + '</p>';
+                localStorage.questaoAtual = 1;
+                document.getElementById("resposta").value = "";
+            } else {
+                alert("Errou!");
+            }
+        }
+
+
+    }
+
+
 
 })
